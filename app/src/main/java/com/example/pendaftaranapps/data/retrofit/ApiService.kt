869695fc83d2fd9b9ef41f9f562fc86970e9eaf3
yusuf <yusufnew060@gmail.com>
2025-02-20 20:29:53 +0700

    CRUD

---
+++ app/src/main/java/com/example/pendaftaranapps/data/retrofit/ApiService.kt
@@ -0,0 +1,43 @@
+package com.example.pendaftaranapps.data.retrofit
+
+import com.example.pendaftaranapps.data.response.AddUpdateResponse
+import com.example.pendaftaranapps.data.response.ListSiswaResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("api-list-siswa.php")
+    fun getAllSiswa(): Call<ListSiswaResponse>
+
+    @FormUrlEncoded
+    @POST("api-add-siswa.php")
+    fun addSiswa(
+        // yang akan kita tampung / terima dibagian response
+        @Field("nama") nama : String,
+        @Field("alamat") alamat : String,
+        @Field("jenis_kelamin") jenisKelamin : String,
+        @Field("agama") agama : String,
+        @Field("sekolah_asal") sekolahAsal : String
+    ) : Call<AddUpdateResponse>
+
+    // kalu api menggunakan form data harus “nama” : String
+    @FormUrlEncoded
+    @POST("api-update-siswa.php")
+    fun updateSiswa(
+        @Query("id") id: Int,
+        @Field("nama") nama: String,
+        @Field("alamat") alamat: String,
+        @Field("jenis_kelamin") jenisKelamin : String,
+        @Field("agama") agama : String,
+        @Field("sekolah_asal") sekolahAsal : String
+    ): Call<AddUpdateResponse>
+
+    @POST("api-delete-siswa.php")
+    fun deleteSiswa(
+        @Query("id") id: Int
+    ): Call<AddUpdateResponse>
+}
---
+++ app/src/main/java/com/example/pendaftaranapps/ui/ListSiswaAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.pendaftaranapps.ui
+
+import android.app.Activity
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pendaftaranapps.AddUpdateActivity
+import com.example.pendaftaranapps.data.response.DataItem
+import com.example.pendaftaranapps.databinding.ItemSiswaBinding
+
+class ListSiswaAdapter : ListAdapter<DataItem, ListSiswaAdapter.ViewHolder>(DIFF_CALLBACK) {
+    class ViewHolder (val binding: ItemSiswaBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(dataItem: DataItem){
+            binding.namaSiswa.text = dataItem.nama
+            binding.asalSekolah.text = dataItem.sekolahAsal
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val dataItem = getItem(position)
+        holder.bind(dataItem)
+
+        //saat di click akan pindah ke AddUpdateActivity
+        val activity = holder.itemView.context as Activity
+        holder.binding.rvSiswa.setOnClickListener {
+            val intent = Intent(activity, AddUpdateActivity::class.java)
+            intent.putExtra(AddUpdateActivity.EXTRA_DATA, dataItem)
+            activity.startActivity(intent)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemSiswaBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
+    }
+
+    companion object{
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<DataItem>(){
+            override fun areContentsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
